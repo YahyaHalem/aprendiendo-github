8e7fbdc0a7e70665bd502a1592c3268607a4d456
Yahya Hasen Halem Morales <yhalemmo@itam.mx>
2025-08-29 17:57:21 -0600

    Primer archivo Python
    Archivo hola-mundo.py con funciones básicas para practicar

---
+++ hola-mundo.py
@@ -0,0 +1,25 @@
+# Mi primer archivo de código em GitHub
+print("¡Hola GitHub!")
+print ("Este es mi primer repositorio")
+
+# Una función simple
+def saludar(nombre):
+  return f"Hola {nombre}, bienvenido a GitHub!"
+
+# Usar la función
+mi_nombre = "Yahya Halem"
+mensaje = saludar(mi_nombre)
+print(mensaje)
+
+#Lista de cosas que he aprendido
+aprendido = [
+  "Crear repositorios",
+  "Hacer commits",
+  "Editar README",
+  "Crear archivos de código"
+]
+
+print("\nHe aprendido: ")
+  for item in aprendido:
+    print(f"✅ {item}")
+    
